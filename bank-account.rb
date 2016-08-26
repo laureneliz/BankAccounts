cde605a86be9c58089ff9822841212a42c8bdf3e
Lauren Brink <laurenfriesbrink@gmail.com>
2016-08-26 15:51:47 -0700

    Wave 3 with MoneyMarket and debugging in Account

--- bank-account.rb
+++ bank-account.rb
@@ -17,94 +17,28 @@ module Bank
       @minimum = 0
       min_balance(@balance, @minimum)
       # run method complete_info
-      # complete_info ######### unhash this once you make the find method!!!!!!!!
-      puts "Account is created with #{print_d(@balance)} balance."
+      complete_info ######### unhash this once you make the find method!!!!!!!!
+      return "Account is created with #{print_d(@balance)} balance."
     end # end of initialize
-
-    def min_balance(balance,minimum)
-      # no putting in negative dollars allowed!!!
-      if balance < minimum
-        raise ArgumentError.new("Bank account cannot be created with less than #{print_d(@minimum)} balance.")
-      end
-    end
-
-    def complete_info
-      # if there is no ID, create one!
-      if @id == nil
-        @id = rand(100000..999999).to_s
-      end
-
-      # if the account is created without an owner, offers user the opportunity to add an owner
-      if @owner == nil
-        puts "Would you like to create an owner for this account? Y/N"
-        owner_or_not = gets.chomp.downcase
-        if owner_or_not == "y" || owner_or_not == "yes"
-          puts "Enter first name, press return. Enter last name, press return."
-          @owner = Owner.new(first_name: gets.chomp, last_name: gets.chomp)
-        else
-          puts "Okay, but this account is now ownerless."
-        end
-      end # end of @owner loop
-
-      # if there is no created date, create one
-      if @created_date == nil
-        @created_date == Time.now
-      end
-    end
-
-    def make_into_cents(money)
-      if Integer(money) != money
-        money = (money * 100).to_i # this stores a float as an int. ie if they \enter 100.25 it multiplies it by 100 and stores that.
-      elsif money.to_s.end_with?(".0")
-        money = (money * 100).to_i # omfg this method is getting so complicated
-      else
-        money = money.to_i        # this WAS just in case there is a float like 2500, but now idk if I need it anymore
-      end
-      return money
-    end
 
     # define a method to withdraw money
     def withdraw(withdrawal) ######## this needs to be in cents, and since there's no gets.chomp I don't think I can do anything to control it????
       fee = 0
-      withdrawal_helper(withdrawal, @minimum, fee)
+      status = withdrawal_helper(withdrawal, @minimum, fee)
+      return status
     end
 
-    def withdrawal_helper(withdrawal, minimum, fee)
-      withdrawal = make_into_cents(withdrawal) # just being safe with these guys
-      fee = make_into_cents(fee)
-      minimum = make_into_cents(minimum)
-      if (@balance - withdrawal - fee) < minimum
-        return "Sorry, your balance is #{print_d(@balance)}. Including a fee of #{print_d(fee)} and withdrawal of #{print_d(withdrawal)}, you cannot withdraw an amount that will make your balance smaller than the #{print_d(@minimum)} minimum."
-      else
-        @balance -= (withdrawal + fee)
-        return"You have withdrawn #{print_d(withdrawal)}. \nThere was a fee of #{print_d(fee)}. \nYour balance is now #{print_d(@balance)}."
-      end
-      return @balance
-    end
-
-    # deposit and dd to blaance
+    # deposit and add to blaance
     def deposit(deposit)
       make_into_cents(deposit)
       # checks to see if deposit is less than 0, if so deposits
       if deposit < 0
-        return "Depositing negative dollars is not allowed."
+        status = "Depositing negative dollars is not allowed."
       else
         @balance += deposit
-        return "You have deposited #{print_d(deposit)}. \nYour balance is now #{print_d(@balance)}."
+        status = "You have deposited #{print_d(deposit)}. \nYour balance is now #{print_d(@balance)}."
       end
-
-      # returns balance
-      return @balance
-    end
-
-    # this makes the cents look nice to print in dollars
-    def print_d(dollars)
-      if dollars.to_s.length <= 3
-        dollars = "$" + sprintf("%03d", dollars).insert(-3, ".") # added this if statement to account for dollar amounts that are less than three characters ($.50 aka 50)
-      else
-        dollars = "$" + dollars.to_s.insert(-3, ".")
-      end
-      return dollars
+      return status
     end
 
     # this creates a whole buncha accounts, reading from a CSV
@@ -134,8 +68,8 @@ module Bank
       @@bank_accounts.each do |account|
           if account.id.to_s == id
             return account
-          # else
-            # puts "Unable to locate an account with that ID" # originally this was a return, but it quit the loop after the first iteration.
+          else
+            puts "Unable to locate an account with that ID" # originally this was a return, but it quit the loop after the first iteration.
           end
       end
     end # end of self.find method
@@ -145,8 +79,75 @@ module Bank
       return "ID: #{@id}\nBalance: #{print_d(@balance)}\nCreated Date: #{@created_date}"
     end
 
-  end # end of Account class
+    #### these methods are private#######
+    private
+    def complete_info
+      # if there is no ID, create one!
+      if @id == nil
+        @id = rand(100000..999999).to_s
+      end
 
+      # if the account is created without an owner, offers user the opportunity to add an owner
+      if @owner == nil
+        puts "Would you like to create an owner for this account? Y/N"
+        owner_or_not = gets.chomp.downcase
+        if owner_or_not == "y" || owner_or_not == "yes"
+          puts "Enter first name, press return. Enter last name, press return."
+          @owner = Owner.new(first_name: gets.chomp, last_name: gets.chomp)
+        else
+          puts "Okay, but this account is now ownerless."
+        end
+      end # end of @owner loop
+
+      # if there is no created date, create one
+      if @created_date == nil
+        @created_date = Time.now
+      end
+    end
+
+    def make_into_cents(money)
+      if Integer(money) != money
+        money = (money * 100).to_i # this stores a float as an int. ie if they \enter 100.25 it multiplies it by 100 and stores that.
+      elsif money.to_s.end_with?(".0")
+        money = (money * 100).to_i # omfg this method is getting so complicated
+      else
+        money = money.to_i        # this WAS just in case there is a float like 2500, but now idk if I need it anymore
+      end
+      return money
+    end
+
+    # this makes the cents look nice to print in dollars
+    def print_d(dollars)
+      if dollars.to_s.length <= 3
+        dollars = "$" + sprintf("%03d", dollars).insert(-3, ".") # added this if statement to account for dollar amounts that are less than three characters ($.50 aka 50)
+      else
+        dollars = "$" + dollars.to_s.insert(-3, ".")
+      end
+      return dollars
+    end
+
+    def min_balance(balance,minimum)
+      # no putting in negative dollars allowed!!!
+      if balance < minimum
+        raise ArgumentError.new("Bank account cannot be created with less than #{print_d(@minimum)} balance.")
+      end
+    end
+
+    # allows you to incorporate a fee and a min balance
+    def withdrawal_helper(withdrawal, minimum, fee)
+      withdrawal = make_into_cents(withdrawal) # just being safe with these guys
+      fee = make_into_cents(fee)
+      minimum = make_into_cents(minimum)
+      if (@balance - withdrawal - fee) < minimum
+        status = "Sorry, your balance is #{print_d(@balance)}. Including a fee of #{print_d(fee)} and withdrawal of #{print_d(withdrawal)}, you cannot withdraw an amount that will make your balance smaller than the #{print_d(@minimum)} minimum."
+      else
+        @balance -= (withdrawal + fee)
+        status = "You have withdrawn #{print_d(withdrawal)}. \nThere was a fee of #{print_d(fee)}. \nYour balance is now #{print_d(@balance)}."
+      end
+      return status
+    end
+
+  end # end of Account class
 
   class Owner
     attr_reader :owner_id, :first_name, :last_name, :street_address, :city, :state
@@ -192,7 +193,7 @@ module Bank
       super # pulls in initialize method from Account classa
       @minimum = 1000 # this means ten dollars!
       min_balance(@balance, @minimum) # checks to make sure that there is enough money
-      return "Account is created with #{print_d(@balance)} balance."
+      puts "Account is created with #{print_d(@balance)} balance."
     end
 
     def withdraw(withdrawal)
@@ -233,43 +234,129 @@ module Bank
       return "#{status}\nYou have used #{@checks_used} check(s) this month."
     end
 
+    private
     def reset_checks
       @checks_used = 0
     end
   end # end of class CheckingAccount
 
+  class MoneyMarket < Account
+    def initialize(account_details_hash)
+      super
+      @minimum = 1000000 #ten thousand buckaroos
+      @max_transactions = 6
+      @transactions_blocked = false
+      @total_transactions = 0
+      min_balance(@balance, @minimum) # checks to make sure that there is enough money
+      status = "Account is created with #{print_d(@balance)} balance.\n**********"
+      puts status
+    end
+
+    def withdraw(withdrawal)
+      withdrawal = make_into_cents(withdrawal)
+      if check_if_blocked?
+        status = "Withdrawal not allowed. \nThere have been #{@total_transactions} transaction this month, and only #{@max_transactions} are allowed per month. \nTransactions are blocked for the remainder of the month."
+      elsif check_if_overdrawn?
+        status = "Withdrawal not allowed. \nTransactions are blocked until balance is #{print_d(@minimum)} or greater. \nPlease deposit funds."
+      else
+        status = mm_withdrawal(withdrawal)
+        @total_transactions += 1
+      end
+    return status
+    end
+
+    def deposit(deposit)
+      deposit = make_into_cents(deposit)
+      if check_if_blocked?
+        status = "Deposit not allowed. \nThere have been #{@total_transactions} transaction this month, and only #{@max_transactions} are allowed per month. \nTransactions are blocked for the remainder of the month."
+      elsif check_if_overdrawn?
+        status = deposit_while_overdrawn(deposit)
+      else
+        @total_transactions += 1
+        status = super
+        #woo hoo, just call parent deposit method
+      end
+      return status
+    end
+
+    ###private methods###
+    def block
+      @transactions_blocked = true
+    end
+
+    def unblock
+      @transactions_blocked = false
+    end
+
+    def reset_transactions
+      @total_transactions = 0
+    end
+
+    def check_if_blocked? # checks if transactions are at max. if they are, returns true. if not, false.
+      if @total_transactions >= 6
+        block
+        return true
+      end
+    end
+
+    def check_if_overdrawn?
+      if @transactions_blocked
+        return true
+      end
+    end
+
+    def mm_withdrawal(withdrawal) ### add logic for depositing til less than zero ZERO!!!
+      withdrawal = make_into_cents(withdrawal)
+      fee = 100
+      if (@balance - withdrawal) >= @minimum
+        fee = 0
+        withdrawal_helper(withdrawal, @minimum, fee)
+      elsif (@balance - withdrawal - fee) <= 0              # this won't allow you to withdraw at all if
+        return "Cannot withdraw #{print_d(withdrawal)}\nBalance is #{print_d(@balance)}. Balance must not be less than $0.00."
+      else
+        block
+        @balance = @balance - withdrawal - fee
+        return "You have withdrawn #{print_d(withdrawal)}.\nYour balance is now #{print_d(@balance)}, which is below the minimum of #{print_d(@minimum)}.\nYour account will be locked until you deposit #{print_d(@minimum - @balance)} or more."
+      end
+    end
+
+    def deposit_while_overdrawn(deposit)
+      deposit = make_into_cents(deposit)
+      if deposit + @balance >= @minimum
+        unblock
+        @balance += deposit
+        return "Your deposit of #{print_d(deposit)} has been accepted and your account has been unblocked. \nBalance is #{print_d(@balance)}."
+        # do not increase transactions!
+      else
+        return "Your deposit must increase your balance to #{print_d(@minimum)} Please deposit #{print_d(@minimum - @balance)} or more."
+      end
+    end
+
+  end # end of MM
+
 end # end of Bank
 
-regular = Bank::Account.new(id:50493, balance:1000)
-puts regular.withdraw(9.50)
-puts "**********************************"
-puppy = Bank::SavingsAccount.new(id: 4, balance: 1000000)
-# puts puppy
-puts puppy.withdraw(90.5)
-# puts puppy.add_interest(0.25)
-puts "**********************************"
-savings = Bank::SavingsAccount.new(id:40584, balance:1400)
-puts savings.withdraw(10.00)
-puts savings.add_interest(0.34)
-puts "**********************************"
-checking = Bank::CheckingAccount.new(id:20449, balance:10495)
-puts checking.withdraw(10.00)
-puts "**********************************"
-puts checking.withdraw_using_check(15.50)
-puts checking.withdraw_using_check(15.89)
-puts checking.withdraw_using_check(4500)
-checking.reset_checks
-puts checking.withdraw_using_check(12.50)
-puts checking.withdraw_using_check(100)
-# kitty = Bank::CheckingAccount.new(id: 103940304, balance: 1000)
-# # puts kitty
-# puts kitty.withdraw(1075)
 
-
-
-
-# ap Bank::Account.all
-
-# puts Bank::Account.find("1217")
-
-# puts Bank::Owner.create_owners
+mm = Bank::MoneyMarket.new(id:5093830, balance:5000000)
+puts mm
+puts mm.created_date
+# puts "*****"
+# puts mm.withdraw(500000) # withdraw 5k, should be 45k 1
+# puts "****"
+# puts mm.withdraw(1000000) # withdraw 10k, should be 35k 2
+# puts "***"
+# puts mm.withdraw(1000000) # withdraw 10k, should be 25k 3
+# puts "****"
+# puts mm.deposit(500000)  #deposit 5k, should be 30k 4
+# puts "****"
+# puts mm.withdraw(500000)  #withdraw 5k, should be 25k 5
+# puts "****"
+# puts mm.withdraw(500000)#withdraw 5k, should be 20k 6
+# puts "****"
+# puts mm.withdraw(5000) # SHOULD NOT BE ALLOWED
+# puts "****"
+# # puts mm.withdraw()
+# puts "****"
+# # puts mm.deposit()
+# puts "****"
+# puts mm.deposit(50000) #should not be allowed
